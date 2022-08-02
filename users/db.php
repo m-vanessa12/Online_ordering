7d3033a80e4c6f9f0c9e80cd9692cec138675cd9
Vanessa Mukamanzi <v.mukamanzi@alustudent.com>
2022-08-02 13:20:53 +0200

    database

--- users/db.php
+++ users/db.php
@@ -4,19 +4,19 @@
 
 session_start();
 
-$dbserver = "localhost";
-$dbusername = "root";
-$dbpassword = "";
-$dbname = "restaurant";
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
-
-$conn = mysqli_connect($dbserver, $dbusername, $dbpassword, $dbname);
-
-if(!$conn){
-    die("Connection failed: ".mysqli_connect_error());
-}
-
-// echo "connection successfully";
 
 ?>
+
+
 
